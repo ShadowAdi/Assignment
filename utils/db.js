71b33a83e307a23ddf819cf72352c94f8d1b7284
Aditya Shukla <Shadowshukla76@gmail.com>
2025-03-26 09:54:14 +0530

    add

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
             console.error(`Error: Connection Do not Exist`);
             process.exit(1); // Exit process with failure
         }
-        const conn = await mongoose.connect(MongoDB_CONNECTION);
+        const conn = await mongoose.connect("mongodb+srv://shadowshukla76:DEl8nOof4NwiItJN@assignmenttesting.umrrt.mongodb.net/?retryWrites=true&w=majority&appName=AssignmentTesting");
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
